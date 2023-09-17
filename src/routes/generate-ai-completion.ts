b1e8a725975ebfe8b2575dcf6097ffee8f19eb3a
lucasilvas2 <lucasoliver1068@gmail.com>
2023-09-17 18:32:12 -0300

    feat: change route generation prompt and created seed

--- src/routes/generate-ai-completion.ts
+++ src/routes/generate-ai-completion.ts
@@ -8,11 +8,11 @@ export async function generateAICompletionRoute(app: FastifyInstance) {
   app.post("/ai/complete", async (req, reply) => {
     const bodySchema = z.object({
       videoId: z.string().uuid(),
-      template: z.string(),
+      prompt: z.string(),
       temperature: z.number().min(0).max(1).default(0.5),
     });
 
-    const { videoId, template, temperature } = bodySchema.parse(req.body);
+    const { videoId, prompt, temperature } = bodySchema.parse(req.body);
 
     const video = await prisma.video.findFirstOrThrow({
       where: {
@@ -26,7 +26,7 @@ export async function generateAICompletionRoute(app: FastifyInstance) {
         .send({ error: "Video transcription was not generated yet." });
     }
 
-    const promptMessage = template.replace(
+    const promptMessage = prompt.replace(
       "{transcription}",
       video.transcription
     );
